65e8eaf87c51780be5be7bdf302dce5e9316bab6
demidraksin <greatraksin@icloud.com>
2021-04-30 10:12:04 +0300

    Adding random functions🥺

---
+++ IV_form/lesson_3004_random/rand_tut.js
@@ -0,0 +1,6 @@
+let randInt = Math.floor(Math.random() * 10);
+// Math.random() * 10 - случайное число от 0 до 9
+let randIntInclude = Math.floor(Math.random() * 10) + 1;
+// Случайное число от 0 до 10
+console.log(randInt);
+console.log(randIntInclude);
