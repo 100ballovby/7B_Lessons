be1e2086af7deb8e356340797e23de63dfaec15e
demidraksin <greatraksin@icloud.com>
2021-04-30 09:55:18 +0300

    Adding JS starter lesson

--- IV_form/lesson_3004_random/script.js
+++ IV_form/lesson_3004_random/script.js
@@ -5,10 +5,11 @@ function clickBtn() {
     let button = this;
     square.style.backgroundColor = '#000000';
     button.setAttribute('disabled', 'true');
-    setTimeout(clearAll, 2000, button);
+    setTimeout(clearAll, 2000);
 }
 
 function clearAll() {
+    let button = clickMe;
     let square = document.getElementById('square');
     square.style.backgroundColor = 'transparent';
     button.removeAttribute('disabled');
